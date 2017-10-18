b7d8f84bc58cec4fc3d425f3a2e156b5514ac7c8
川合 俊介 <Shunsuke_Kawai@jmas.co.jp>
2017-10-18 18:21:19 +0900

    delete othre sln

--- WebServices/TodoAzurePush/TodoAzure/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TodoAzure
-{
-    public static class Constants
-    {
-        // Replace string with your mobile service URL.
-        public static string ApplicationURL = @"<INSERT_MOBILE_APP_URL_HERE>";
-    }
-}
